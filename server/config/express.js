79361a522835378d9aa54c196fab4726101d52fc
Lucian Maran <lucian.maran@outlook.com>
2015-09-28 23:08:33 +0300

    clean up auth related server-side code

--- server/config/express.js
+++ server/config/express.js
@@ -3,10 +3,7 @@
 var express = require('express');
 var favicon = require('serve-favicon');
 var morgan = require('morgan');
-var compression = require('compression');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var cookieParser = require('cookie-parser');
 var errorHandler = require('errorhandler');
 var path = require('path');
 var config = require('./environment');
@@ -20,11 +17,8 @@ module.exports = function(app) {
     
     app.set('views', config.root + '/server/views');
     app.set('view engine', 'jade');
-    app.use(compression());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
-    app.use(methodOverride());
-    app.use(cookieParser());
     app.use(passport.initialize());
 
     // Persist sessions with mongoStore
@@ -42,8 +36,6 @@ module.exports = function(app) {
     if ('production' === env || 'staging' === env) {
         app.use(favicon(path.join(config.root, 'client', 'favicon.ico')));
         
-        // app.use(express.static(path.join(config.root, 'public')));
-        // app.set('appPath', path.join(config.root, 'public'));
         app.use(express.static(path.join(config.root, 'client')));
         app.set('appPath', path.join(config.root, 'client'));    
         
