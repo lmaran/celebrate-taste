7440efe9f747acbb1182d384a8931ecd358f713d
Lucian Maran <lucian.maran@outlook.com>
2015-10-04 15:19:51 +0300

    fix: in mobil mode, do not close navbar when a dropdown link is pressed

--- client/app/page1/page1Routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-		.when('/page1', {
-            controller: 'page1Controller',
-            templateUrl: 'app/page1/page1.html'
-		});
-}]);
