08a4e17436793447507b7b667b5de8fd7c13c606
Lucian Maran <lucian.maran@outlook.com>
2015-10-15 18:37:56 +0300

    add CRUD for Menu (first version)

---
+++ client/app/menu/menuController.js
@@ -0,0 +1,144 @@
+﻿/*global app*/
+'use strict';
+
+app.controller('menuController', ['$scope', '$window', '$route', 'menuService', '$location', function ($scope, $window, $route, menuService, $location) {
+    $scope.isEditMode = $route.current.isEditMode;
+    $scope.isFocusOnName = $scope.isEditMode ? false : true;
+
+    $scope.menu = {};
+   
+    if ($scope.isEditMode) {  
+        /*jshint latedef: nofunc */ // https://jslinterrors.com/a-was-used-before-it-was-defined     
+        init(); 
+    }
+
+    function init() {
+        getCustomer();
+    } 
+
+    function getCustomer() {
+        menuService.getById($route.current.params.id).then(function (data) {
+            $scope.menu = data;
+        })
+        .catch(function (err) {
+            alert(JSON.stringify(err, null, 4));
+        });
+    }
+
+    $scope.create = function (form) {
+        $scope.submitted = true;
+        if (form.$valid) {
+            //alert(JSON.stringify($scope.menu));
+            menuService.create($scope.menu)
+                .then(function (data) {
+                    $location.path('/menus');
+                    //Logger.info("Widget created successfully");
+                })
+                .catch(function (err) {
+                    alert(JSON.stringify(err.data, null, 4));
+                });
+        }
+    };
+
+    $scope.update = function (form) {
+        $scope.submitted = true;
+        if (form.$valid) {
+            //alert(JSON.stringify($scope.menu));
+            menuService.update($scope.menu)
+                .then(function (data) {
+                    $location.path('/menus');
+                    //Logger.info("Widget created successfully");
+                })
+                .catch(function (err) {
+                    alert(JSON.stringify(err.data, null, 4));
+                });
+        }
+    };
+
+    $scope.cancel = function () {
+        //$location.path('/widgets')
+        $window.history.back();
+    }
+    
+
+
+
+
+
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+  $scope.maxDate = new Date(2020, 5, 22);
+
+  $scope.open = function($event) {
+      alert(123);
+    $scope.status.opened = true;
+  };
+
+  $scope.setDate = function(year, month, day) {
+    $scope.dt = new Date(year, month, day);
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+
+  $scope.status = {
+    opened: false
+  };
+
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  var afterTomorrow = new Date();
+  afterTomorrow.setDate(tomorrow.getDate() + 2);
+  $scope.events =
+    [
+      {
+        date: tomorrow,
+        status: 'full'
+      },
+      {
+        date: afterTomorrow,
+        status: 'partially'
+      }
+    ];
+
+  $scope.getDayClass = function(date, mode) {
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+      for (var i=0;i<$scope.events.length;i=i+1){
+        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
+        }
+      }
+    }
+
+    return '';
+  };
+
+
+
+
+}]);
---
+++ server/api/menu/menuService.js
@@ -0,0 +1,50 @@
+﻿'use strict';
+
+(function (menuService) {
+    
+    //var seedData = require("./seedData");
+    var mongoHelper = require('../../data/mongoHelper');
+ 
+    menuService.getAll = function (next) {      
+        mongoHelper.getDb(function (err, db) {
+            if (err) return next(err, null);
+            db.menus.find().toArray(function (err, docs) {
+                if (err) return next(err, null);
+                return next(null, docs);                 
+            });
+        });
+    };
+
+    menuService.getById = function (id, next) {
+        mongoHelper.getDb(function (err, db) {
+            if (err) return next(err, null);
+            id = mongoHelper.normalizedId(id);
+            db.menus.findOne({ _id: id }, next);                           
+        });
+    };
+
+    menuService.create = function (menu, next) {
+        mongoHelper.getDb(function (err, db) {
+            if (err) return next(err, null);
+            db.menus.insertOne(menu, next);      
+        });
+    };
+
+    menuService.update = function (menu, next) {
+        mongoHelper.getDb(function (err, db) {
+            if (err) return next(err, null);
+            menu._id = mongoHelper.normalizedId(menu._id);
+            // returnOriginal: (default:true) Set to false if you want to return the modified object rather than the original
+            db.menus.findOneAndUpdate({_id:menu._id}, menu, {returnOriginal: false}, next);
+        });
+    };  
+
+    menuService.remove = function (id, next) {
+        mongoHelper.getDb(function (err, db) {
+            if (err) return next(err, null);
+            id = mongoHelper.normalizedId(id);               
+            db.menus.findOneAndDelete({_id:id}, next);
+        });
+    };
+    
+})(module.exports);
