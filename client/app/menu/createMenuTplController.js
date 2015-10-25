d71ec20fbe89a6a3813e89cbb5f1ff1eaf31f67c
Lucian Maran <lucian.maran@outlook.com>
2015-10-25 09:52:22 +0200

    add new menu; delete menu

---
+++ client/app/menu/createMenuTplController.js
@@ -0,0 +1,38 @@
+'use strict';
+
+app.controller('createMenuTplController', ['$scope', '$uibModalInstance', 'dataToModal', 'dayTimeService', 
+	function ($scope, $uibModalInstance, dataToModal, dayTimeService) {
+
+    // $scope.items = dataToModal;
+    // $scope.selected = {
+    //     item: $scope.items[0]
+    // };
+    if(dataToModal && dataToModal.length === 10)
+        $scope.menuDate = dayTimeService.getDateFromString(dataToModal); // "yyyy-mm-dd"
+    else
+        $scope.menuDate = new Date();
+        
+    $scope.menuDate.setDate($scope.menuDate.getDate() + 1); // increment date
+    
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.menuDate);        
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+    
+    
+   // ****** calendar settings ****** 
+    
+    $scope.dateOptions = {
+        startingDay: 1,
+    };
+
+    $scope.isOpen = false;
+    $scope.open = function () {
+        $scope.isOpen = true;
+    };   
+ 
+    
+}]);
