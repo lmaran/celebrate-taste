016fe3fec1ffe92c769f32c2a57a178ad8cb3fbe
Lucian Maran <lucian.maran@outlook.com>
2015-10-28 23:24:09 +0200

    edit item in menu; other UI adjustments

--- client/app/menu/menuItemController.js
+++ client/app/menu/menuItemController.js
@@ -1,13 +1,16 @@
 ﻿/*global app*/
+/* global _ */
 'use strict';
 
-app.controller('dishController', ['$scope', '$window', '$route', 'dishService', '$location', 
-    function ($scope, $window, $route, dishService, $location) {
+app.controller('menuItemController', ['$scope', '$window', '$route', 'menuService', 
+    function ($scope, $window, $route, menuService) {
         
     $scope.isEditMode = $route.current.isEditMode;
     $scope.isFocusOnName = $scope.isEditMode ? false : true;
 
+    $scope.menu = {};
     $scope.dish = {};
+    
    
     if ($scope.isEditMode) {  
         /*jshint latedef: nofunc */ // https://jslinterrors.com/a-was-used-before-it-was-defined     
@@ -15,39 +18,40 @@ app.controller('dishController', ['$scope', '$window', '$route', 'dishService', 
     }
 
     function init() {
-        getCustomer();
+        getMenu();
     } 
 
-    function getCustomer() {
-        dishService.getById($route.current.params.id).then(function (data) {
-            $scope.dish = data;
+    function getMenu() {
+        menuService.getById($route.current.params.menuId).then(function (data) {
+            $scope.menu = data;
+            $scope.dish = _.find(data.dishes, '_id', $route.current.params.dishId);
         })
         .catch(function (err) {
             alert(JSON.stringify(err, null, 4));
         });
     }
 
-    $scope.create = function (form) {
-        $scope.submitted = true;
-        if (form.$valid) {
-            //alert(JSON.stringify($scope.dish));
-            dishService.create($scope.dish)
-                .then(function (data) {
-                    //$location.path('/dishes');
-                    $window.history.back();
-                    //Logger.info("Widget created successfully");
-                })
-                .catch(function (err) {
-                    alert(JSON.stringify(err.data, null, 4));
-                });
-        }
-    };
-
+//     $scope.create = function (form) {
+//         $scope.submitted = true;
+//         if (form.$valid) {
+//             //alert(JSON.stringify($scope.dish));
+//             dishService.create($scope.dish)
+//                 .then(function (data) {
+//                     //$location.path('/dishes');
+//                     $window.history.back();
+//                     //Logger.info("Widget created successfully");
+//                 })
+//                 .catch(function (err) {
+//                     alert(JSON.stringify(err.data, null, 4));
+//                 });
+//         }
+//     };
+// 
     $scope.update = function (form) {
         $scope.submitted = true;
         if (form.$valid) {
             //alert(JSON.stringify($scope.dish));
-            dishService.update($scope.dish)
+            menuService.update($scope.menu)
                 .then(function (data) {
                     //$location.path('/dishes');
                     $window.history.back();
