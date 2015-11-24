b986ad8fb2928c85e513d71813f341a99ee02e83
lmaran <lucian.maran@outlook.com>
2015-11-24 18:18:52 +0000

    move server-side static files into a separate folder [skip ci]

--- server/routes.js
+++ server/routes.js
@@ -26,7 +26,7 @@ module.exports = function(app) {
     app.route('/contact2')
     .get(function(req,res){
         res.render('contact');
-    });     
+    });
   
     // All undefined asset or api routes should return a 404
     app.route('/:url(api|auth|components|app|bower_components|assets)/*')
