ce18946872a8f00c1dfd0b03fb6aae408a0e3dd7
lmaran <lucian.maran@outlook.com>
2015-11-28 13:09:41 +0000

    cleanup routing files [skip ci]

--- server/api/customerEmployee/customerEmployeeRoutes.js
+++ server/api/customerEmployee/customerEmployeeRoutes.js
@@ -2,18 +2,12 @@
 
 var express = require('express');
 var controller = require('./customerEmployeeController');
-
 var router = express.Router();
 
 router.get('/', controller.getAll);
 router.get('/:id', controller.getById);
 router.post('/', controller.create);
-
-// router.put('/:id', controller.update);
-// router.patch('/:id', controller.update);
 router.put('/', controller.update);
-router.patch('/', controller.update);
-
 router.delete('/:id', controller.remove);
 
 module.exports = router;
--- server/api/items/itemsService.js
+++
@@ -1,8 +0,0 @@
-(function (itemsService) {
-
-    itemsService.getAll = function (next) {
-        next({name:'myName', value:12345});
-    };
-
-
-})(module.exports);
