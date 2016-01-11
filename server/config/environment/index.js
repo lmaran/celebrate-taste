ed2a3a0d22eabf4056093c4b11432397d0158ee5
lmaran <lucian.maran@outlook.com>
2016-01-11 21:31:10 +0000

    Added: Google Analytics, Rollbar, Winston; update npm packages [skip ci]

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -15,27 +15,29 @@ function requiredProcessEnv(name) {
 // All configurations will extend these options
 // ============================================
 var all = {
-  env: process.env.NODE_ENV,
+    env: process.env.NODE_ENV,
 
-  // Root path of server
-  root: path.normalize(__dirname + '/../../..'), // 3 folders back from the current folder
-      // Secret for session, you will want to change this and make it an environment variable
+    // Root path of server
+    root: path.normalize(__dirname + '/../../..'), // 3 folders back from the current folder
+
+    // Secret for session, you will want to change this and make it an environment variable
     secrets: {
         session: 'node-fullstack-secret'
     },
+    
     // List of user roles
     userRoles: ['guest', 'user', 'admin'],
+    
+    // MongoDB connection options
+    mongo: {
+        options: {
+            db: {
+                //safe: true // in Mongo 2.0 this option is 'true' by default and is equals to {w:1} - http://stackoverflow.com/a/14801527
+            }
+        }
+    },
 
-
-  // MongoDB connection options
-  mongo: {
-    options: {
-      db: {
-        //safe: true // in Mongo 2.0 this option is 'true' by default and is equals to {w:1} - http://stackoverflow.com/a/14801527
-      }
-    }
-  },
-
+    rollbarToken: 'c40dd41c292340419923230eed1d0d61'
 };
 
 // Export the config object based on the NODE_ENV
