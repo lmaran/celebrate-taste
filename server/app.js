d6221d65671349db1cb2a7fc20abc992c3c9622f
lmaran <lucian.maran@outlook.com>
2016-01-16 21:48:14 +0000

    refactoring error and http logging [skip ci]

--- server/app.js
+++ server/app.js
@@ -8,18 +8,18 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var config = require('./config/environment');
-var logger = require("./utils/logger");
+var logger = require("./logging/logger");
+var errorLogHandler = require("./logging/errorLogHandler");
 
-// Setup server
 var app = express();
 
 require('./config/express')(app);
 
 require('./routes')(app);
 
-// Handle error has to be last, after other app.use() and routes calls;" - http://expressjs.com/en/guide/error-handling.html
-require('./config/errorHandler')(app);
-    
+// Handle error has to be last: http://expressjs.com/en/guide/error-handling.html
+app.use(errorLogHandler());
+
 // Start server
 app.listen(config.port, config.ip, function () {
     logger.info('Express server listening on %d in %s mode', config.port, config.env);
