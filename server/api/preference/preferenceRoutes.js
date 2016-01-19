d19d48696ebe29715d42763030227f7d870c542d
lmaran <lucian.maran@outlook.com>
2016-01-19 22:57:21 +0000

    added "filter by preferences" for orders [skip ci]

--- server/api/preference/preferenceRoutes.js
+++ server/api/preference/preferenceRoutes.js
@@ -6,8 +6,7 @@ var router = express.Router();
 
 router.post('/', controller.create);
 router.post('/createMany', controller.createMany);
-router.get('/', controller.getByDate);
-router.get('/employee/:employeeName', controller.getNextByEmployee);
+router.get('/', controller.getAll);
 router.get('/nextDates', controller.getNextDates); //returns a list of available dates
 router.get('/\\$count', function(req, res) {req.params.$count = true; controller.getAll(req, res); });
 router.get('/:id', controller.getById);
