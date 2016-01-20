966f373faa18711b9b20b51fc427027668af90fa
lmaran <lucian.maran@outlook.com>
2016-01-20 10:38:36 +0000

    fix: today and next menus are not displayed on the public site [skip ci]

--- server/data/dateTimeHelper.js
+++ server/data/dateTimeHelper.js
@@ -13,6 +13,16 @@
 		else if(dayOfWeek === 5) return 'vineri'; 
 		else if(dayOfWeek === 6) return 'sambata';
 	};
+    
+	factory.getRoShortDay = function(dayOfWeek){
+		if(dayOfWeek === 0) return 'Du';
+		else if(dayOfWeek === 1) return 'Lu';
+		else if(dayOfWeek === 2) return 'Ma'; 
+		else if(dayOfWeek === 3) return 'Mi'; 
+		else if(dayOfWeek === 4) return 'Jo'; 
+		else if(dayOfWeek === 5) return 'Vi'; 
+		else if(dayOfWeek === 6) return 'Sa';
+	};    
 	
 	factory.getRoMonth = function(monthOfYear){
 		if(monthOfYear === 0) return 'ianuarie';
@@ -28,6 +38,21 @@
 		else if(monthOfYear === 10) return 'noiembrie'; 
 		else if(monthOfYear === 11) return 'decembrie';           
 	}; 
+    
+	factory.getRoShortMonth = function(monthOfYear){
+		if(monthOfYear === 0) return 'Ian.';
+		else if(monthOfYear === 1) return 'Feb.';
+		else if(monthOfYear === 2) return 'Mar.'; 
+		else if(monthOfYear === 3) return 'Apr.'; 
+		else if(monthOfYear === 4) return 'Mai'; 
+		else if(monthOfYear === 5) return 'Iun.'; 
+		else if(monthOfYear === 6) return 'Iul.';
+		else if(monthOfYear === 7) return 'Aug.';
+		else if(monthOfYear === 8) return 'Sep.'; 
+		else if(monthOfYear === 9) return 'Oct.'; 
+		else if(monthOfYear === 10) return 'Nov.'; 
+		else if(monthOfYear === 11) return 'Dec.';           
+	};     
 	
 	factory.getFriendlyDate = function(date){ // javascript date object
 		var d = date.getDate();
@@ -38,12 +63,15 @@
         if(dd < 10) dd = '0' + d;
         
         var mm = m;
-        if(mm < 10) m = '0' + m; 
+        if(mm < 10) mm = '0' + m; 
 		
         return{
             dayAsString: this.getRoDay(date.getDay()), // Joi
-            dayOfMonth:dd, // 07, 24
+			dayAsShortString: this.getRoShortDay(date.getDay()), // Jo
+            dayOfMonth:dd, // 07, 24            
+
             monthAsString:this.getRoMonth(m-1), // Aprilie
+            monthAsShortString:this.getRoShortMonth(m-1), // Apr.
             year:yyyy, // 2015
 			ymd: yyyy + '-' + mm + '-' + dd, // 2015-07-23
 			dmy: dd + '.' + mm + '.' + yyyy // 23.07.2015
