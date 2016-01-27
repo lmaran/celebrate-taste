6f5c1baf16255ba6c770c8d3b41a34a04e253c58
Lucian Maran <lucian.maran@outlook.com>
2016-01-28 00:32:55 +0200

    print orders by eatSeries

--- server/api/order/orderLine/orderLineController.js
+++ server/api/order/orderLine/orderLineController.js
@@ -131,8 +131,20 @@ exports.import = function(req, res){
                     };
                     
                     if(employee && employee.badgeCode) orderLine.badgeCode = employee.badgeCode;
-                    if(preference && preference.option1) orderLine.option1 = preference.option1;
-                    if(preference && preference.option2) orderLine.option2 = preference.option2;
+                    if(preference && preference.option1){ 
+                        orderLine.option1 = preference.option1;
+                    } else {
+                        // TODO
+                        // orderLine.option1 = getOption1();
+                        // orderLine.option1IsAuto = true;
+                    }
+                    if(preference && preference.option2) {
+                        orderLine.option2 = preference.option2;
+                    } else {
+                        // TODO
+                        // orderLine.option2 = getOption2();
+                        // orderLine.option1IsAuto = true;                        
+                    }
                     
                     orderLines.push(orderLine);
                 });
@@ -150,43 +162,94 @@ exports.import = function(req, res){
 };
 
 exports.print = function (req, res) {
-    var orderId = req.params.id;
     var opCode = req.params.opCode;
-    // orderLineService.getById(orderLineId, function (err, orderLine) {
-    //     if(err) { return handleError(res, err); }
-    //     res.json(orderLine);
-    // });
-     
-
     
     if(opCode == 'seria1') printSeries(req, res, 'Seria 1');
     if(opCode == 'seria2') printSeries(req, res, 'Seria 2');
     if(opCode == 'seria3') printSeries(req, res, 'Seria 3');
-    if(opCode == 'summary') printSummary(req, res);
-    
-    // 
-    // orderLineService.getById(orderId, function (err, order) {
-    //     if(err) { return handleError(res, err); }
-    //     
-    // });          
-    // 
-           
+    if(opCode == 'summary') printSummary(req, res);       
 };
 
-function printSeries(req, res, seriesName){
+function printSeries(req, res, eatSeries){
+    var orderId = req.params.id;
     
-    orderLineService.getAll(orderId, function (err, orderLines) {
+    orderLineService.getByOrderIdAndSeries(orderId, eatSeries, function (err, orderLines) {
         if(err) { return handleError(res, err); }
-        res.status(200).json(orderLines);        
+        
+        var orderDate = orderLines[0].orderDate;
+        var eatSeries = orderLines[0].eatSeries;
+        
+        //res.status(200).json(orderLines);
+        
+        var _ = require('lodash');
+        var PDFDocument = require('pdfkit'); 
+        var helper = require('../../../data/dateTimeHelper');                 
+
+        var doc = new PDFDocument();
+        
+        doc.fontSize(30)
+            .fontSize(18)
+            .text("Comanda pentru " + eatSeries, {align:'center'})
+            .fontSize(12)
+            //.text(helper.getFriendlyDate(firstDay).dmy + '  -  ' + helper.getFriendlyDate(lastDay).dmy, {align:'center'});
+            .text(helper.getStringFromString(orderDate), {align:'center'})
+            .moveDown(2);
+                    
+        
+        _.chain(orderLines)
+            .map(function(orderLine, idx){
+                var orderLineTxt = orderLine.employeeName;
+                var idxCol = _.padEnd(idx + 1 + '.', 6);
+                // 
+                // if(dish.option) dishTitle = dish.option + '. ' + dish.name;  
+                // if(dish.isFasting) dishTitle = dishTitle + ' (Post)'; 
+                // if(dish.calories) dishTitle = dishTitle + ' - ' + dish.calories + ' calorii';         
+                doc.text(idxCol , {continued: true});
+                doc.text(orderLineTxt, {paragraphGap:8, continued: true});
+                doc.text(', ' + (orderLine.option1 || '-') + ' / ' + (orderLine.option2 || '-'));
+            })
+            .value();
+        
+        // doc.fontSize(15)
+        //     .moveDown(7)
+        //     .text("Va dorim pofta buna! ", {align:'right'});
+
+        
+        res.set('Content-Type', 'application/pdf');
+        doc.pipe(res);
+        doc.end(); 
+        
+        getOption1();                  
     });    
     
-    console.log(seriesName);
-    res.json(seriesName);    
+    // console.log(seriesName);
+    // res.json(seriesName);    
 }
 
 function printSummary(req, res){
     console.log('symmary');
     res.json('symmary');    
+}
+
+
+function getOption1(){
+    // TODO
+    //http://www.javascriptkit.com/javatutors/weighrandom2.shtml 
+    var fruits=["Apples", "Oranges", "Grapes", "Bananas"]
+    var fruitweight=[2, 3, 1, 4] //weight of each element above
+    var totalweight=eval(fruitweight.join("+")) //get total weight (in this case, 10)
+    var weighedfruits=new Array() //new array to hold "weighted" fruits
+    var currentfruit=0
+    
+    while (currentfruit<fruits.length){ //step through each fruit[] element
+        for (var i=0; i<fruitweight[currentfruit]; i++)
+            weighedfruits[weighedfruits.length]=fruits[currentfruit]
+        currentfruit++
+    }
+    console.log(weighedfruits);
+    var randomnumber=Math.floor(Math.random()*totalweight);
+    console.log(randomnumber);
+    console.log(weighedfruits[randomnumber]);
 }
 
 function handleError(res, err) {
