695b1c1da37278bb0c4d93540d5d5f4cdd0d2c60
Lucian Maran <lucian.maran@outlook.com>
2016-02-04 22:12:47 +0200

    starting with "deliveries"

---
+++ client/app/delivery/deliveryLine/orderLineService.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+app.factory('orderLineService', ['$http', function ($http) {
+
+    var factory = {};
+    var rootUrl = '/api/orders/';
+    var orderLinesPart = '/orderLines/';
+
+    factory.create = function (orderId, orderLine) {
+        return $http.post(rootUrl + orderId + orderLinesPart, orderLine);
+    };   
+
+    factory.getAll = function (orderId) {
+        return $http.get(rootUrl + orderId + orderLinesPart).then(function (result) {
+            return result.data;
+        });
+    };
+
+    factory.getById = function (orderId, orderLineId) {
+        return $http.get(rootUrl + orderId + orderLinesPart + orderLineId).then(function (result) {
+            return result.data;
+        });
+    };
+
+    factory.update = function (orderId, orderLine) {
+        return $http.put(rootUrl + orderId + orderLinesPart, orderLine);
+    };
+
+    factory.delete = function (orderId, orderLineId) {
+        return $http.delete(rootUrl + orderId + orderLinesPart + orderLineId);
+    };
+    
+    factory.import = function (orderId, importData) {
+        return $http.post(rootUrl + orderId + orderLinesPart + 'import', importData);
+    };     
+
+    return factory;
+}]);
