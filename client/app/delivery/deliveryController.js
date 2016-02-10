a28056aabadaf351dd7f2d8dc5c6fe93065ab2ce
Lucian Maran <lucian.maran@outlook.com>
2016-02-11 00:21:09 +0200

    implemented: delivery/revoke by card; deliver/revoke manualy; search filters

--- client/app/delivery/deliveryController.js
+++ client/app/delivery/deliveryController.js
@@ -9,22 +9,16 @@ app.controller('deliveryController', ['$scope', '$route', 'deliveryService', '$l
     $scope.isFocusOnName = $scope.isEditMode ? false : true;
     $scope.errors = {};
     $scope.delivery = {};
-    
+    $scope.preferences=['A', 'B', 'C', 'D'];
+    $scope.selectedPreference = 'Toate pref.';
     
     $scope.obj={};
     $scope.obj.isFocusOnBadge = true;
    
-    /*jshint latedef: nofunc */ // https://jslinterrors.com/a-was-used-before-it-was-defined     
     init();     
 
     function init() {
         getDelivery();
-        
-        // var searchObject = $location.search();
-        // if(searchObject.onlyNoBadges)
-        //     $scope.obj.onlyNoBadges = true;  
-        // else
-        //     $scope.obj.onlyNoBadges = false;        
     } 
     
     $scope.refresh = function () {
@@ -37,11 +31,21 @@ app.controller('deliveryController', ['$scope', '$route', 'deliveryService', '$l
         deliveryService.getById($route.current.params.id).then(function (data) {
             $scope.delivery = data;
             $scope.dateAsShortString = dt($scope.delivery.orderDate).dateAsShortString;
+            getMenus($scope.delivery.orderDate);
         })
         .catch(function (err) {
             alert(JSON.stringify(err, null, 4));
         })
     } 
+    
+    function getMenus(orderDate){
+        menuService.getMenu(orderDate).then(function (data) {
+            $scope.menu = data[0];         
+        })
+        .catch(function (err) {
+            alert(JSON.stringify(err, null, 4));
+        }) 
+    }   
     
     function getEatSeriesDetails(orderId, eatSeries, sortByDeliveryDate){
         orderLineService.getEatSeriesDetails(orderId, eatSeries).then(function (data) {
@@ -57,13 +61,24 @@ app.controller('deliveryController', ['$scope', '$route', 'deliveryService', '$l
     } 
    
     $scope.openDeliveryLine = function(orderLine){
+        var templateUrl = 'app/delivery/openToCompleteTpl.html';
+        if(orderLine.status === 'completed'){
+            templateUrl = 'app/delivery/openToRevokeTpl.html';
+        }         
+        
+        setDishOptions(orderLine);
+           
         var modalInstance = $uibModal.open({
             animation:false,
-            templateUrl: 'app/delivery/openDeliveryLineTpl.html',
+            templateUrl: templateUrl,
             controller: 'openDeliveryLineTplController',
             resolve: {
                 dataToModal: function () {
-                    return orderLine;
+                    return {
+                        orderLine: orderLine,
+                        option1: $scope.option1,
+                        option2: $scope.option2
+                    }
                 }
             }            
         });
@@ -96,6 +111,7 @@ app.controller('deliveryController', ['$scope', '$route', 'deliveryService', '$l
     }
     
     $scope.deliverByBadge = function(form){
+        $scope.orderLine = undefined; // clean up the screen
         orderLineService.getOrderLinesByBadge($scope.delivery.orderId, $scope.delivery.eatSeries, $scope.obj.badgeCode).then(function (data) {
             if(data.length === 0){
                 $scope.errorValidation = true;
@@ -107,33 +123,77 @@ app.controller('deliveryController', ['$scope', '$route', 'deliveryService', '$l
                 $scope.errorValidation = false;
                 $scope.orderLine = data[0];
                 
-                toastr.success('Livrarea a fost inregistrata cu succes!');
-                
-                if($scope.menu){
-                    setDishOptions();
-                } else{
-                    menuService.getMenu($scope.orderLine.orderDate).then(function (data) {
-                        $scope.menu = data[0]; // cache menu items
-                        setDishOptions();
-                    })                    
+                if($scope.orderLine.status !== 'completed'){
+                    // set orderLine as 'completed'
+                    var orderLineClone = {};
+                    angular.copy($scope.orderLine, orderLineClone); // deep copy (to deal with status and 'strike-through')
+        
+                    orderLineClone.status = 'completed';
+                    orderLineClone.deliveryDate = new Date();                                            
+                    orderLineService.update(orderLineClone)
+                        .then(function (data) {
+                            toastr.success('Livrarea a fost inregistrata!');
+                        })
+                        .catch(function (err) {
+                            alert(JSON.stringify(err.data, null, 4)); 
+                        })                    
                 }
+                
+                setDishOptions($scope.orderLine);
             }
-            $scope.obj.badgeCode = ''; // reset badgeCode in UI                                     
+            $scope.obj.badgeCode = ''; // reset badgeCode in UI
+            $scope.obj.isFocusOnBadge = true;                                   
         })
         .catch(function (err) {
             alert(JSON.stringify(err, null, 4));
         });                 
     }
     
-    function setDishOptions(){
-        $scope.option1 = _.find($scope.menu.dishes, {option: $scope.orderLine.option1});
-        $scope.option2 = _.find($scope.menu.dishes, {option: $scope.orderLine.option2});
+    // return an orderLine back to open status
+    $scope.revoke = function (orderLine) { 
+        orderLine.status = 'open';
+        delete orderLine.deliveryDate;
+                
+        orderLineService.update(orderLine)
+            .then(function (data) {
+                toastr.success('Livrarea a fost anulata!');
+                $scope.orderLine = undefined; // clean up the screen
+                $scope.obj.isFocusOnBadge = true;
+            })
+            .catch(function (err) {
+                alert(JSON.stringify(err.data, null, 4)); 
+            })                    
+    };     
+    
+    function setDishOptions(orderLine){
+        $scope.option1 = _.find($scope.menu.dishes, {option: orderLine.option1});
+        $scope.option2 = _.find($scope.menu.dishes, {option: orderLine.option2});
     } 
     
     $scope.badgesFilter = function(orderLine){
         if($scope.obj.onlyNoBadges){
             return !orderLine.badgeCode;
         } else return true;
-    }              
+    }
+    
+    $scope.selectPreference = function(preference){
+        if(preference === 'Toate pref.'){
+            $scope.selectedPreference = 'Toate pref.';
+        } else {
+            $scope.selectedPreference = preference;
+        }
+    }    
+    
+    $scope.preferencesFilter = function(orderLine){
+        if($scope.selectedPreference === 'Toate pref.'){
+            return true;
+        } else if ($scope.selectedPreference === 'A' || $scope.selectedPreference === 'B'){
+            return orderLine.option1 === $scope.selectedPreference;
+        } else if ($scope.selectedPreference === 'C' || $scope.selectedPreference === 'D'){
+            return orderLine.option2 === $scope.selectedPreference;
+        } else if($scope.selectedPreference === 'Fara pref.'){
+            return !(orderLine.option1 && orderLine.option2);
+        }
+    }                
 
 }])
