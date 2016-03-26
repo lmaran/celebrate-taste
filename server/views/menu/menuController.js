8bd0e651d7a569565160135396cffbc058f28321
lmaran <lucian.maran@outlook.com>
2016-03-26 17:43:26 +0000

    bug fix when render an empty todas\'s menu [skip ci]

--- server/views/menu/menuController.js
+++ server/views/menu/menuController.js
@@ -9,75 +9,69 @@
     var _ = require('lodash');
     
     menuController.renderTodaysMenu = function (req, res, next) { 
-        var todayStr = helper.getRoTodayStr(); // "2016-03-26" (UTC + 2)
-                  
+        var todayStr = helper.getRoTodayStr(); // "2016-03-26"       
         menuService.getTodaysMenu(todayStr, function (err, menu) {
             if(err) { return handleError(res, err); }
 
             var menuHasDishes = menu && menu.dishes && (menu.dishes.length > 0);
             if(menuHasDishes){                
                 menu.dishes = _.sortBy(menu.dishes, ['category', 'option']);
+                
+                if(req.user && req.user.role === "user"){
+                    
+                    // TODO: run this query in paralel with "getTodaysMenu" 
+                    preferenceService.getByEmployeeAndDate(req.user.name, todayStr, function(err, pref) {
+                        if(err) { return handleError(res, err); }
+
+                        menu.dishes.forEach(function(dish) {
+                            
+                            var dishesInCategory = _.filter(menu.dishes, {'category': dish.category});
+                            
+                            if(dishesInCategory.length > 1){
+                                //var pref = _.find(preferences, {'date': menu.menuDate});
+                                
+                                if(pref){
+                                    menu.preferenceId = pref._id;
+                                };
+
+                                if(dish.category === "1"){
+                                    if(pref && pref.option1 === dish.option){
+                                        dish.isMyOption = true;
+                                    } else {
+                                        dish.isNotMyOption = true;
+                                    }
+                                }
+
+                                if(dish.category === "2"){
+                                    if(pref && pref.option2 === dish.option){
+                                        dish.isMyOption = true;
+                                    } else {
+                                        dish.isNotMyOption = true;
+                                    }
+                                }
+                            }
+                            
+                        });
+
+                    });
+                };              
             };
             
             var context = {
                 user: req.user,
                 menu: menu,
                 today: helper.getStringFromString(todayStr),
-                menuHasDishes: menuHasDishes,
-                
-                tmpRoToday: helper.getRoToday(),
-                tmpRoTodayStr: helper.getRoTodayStr()
+                menuHasDishes: menuHasDishes
             };
-
-            if(req.user && req.user.role === "user"){
-                
-                // TODO: run this query in paralel with "getTodaysMenu" 
-                preferenceService.getByEmployeeAndDate(req.user.name, todayStr, function(err, pref) {
-                    if(err) { return handleError(res, err); }
-
-                    menu.dishes.forEach(function(dish) {
-                        
-                        var dishesInCategory = _.filter(menu.dishes, {'category': dish.category});
-                        
-                        if(dishesInCategory.length > 1){
-                            //var pref = _.find(preferences, {'date': menu.menuDate});
-                            
-                            if(pref){
-                                menu.preferenceId = pref._id;
-                            };
-
-                            if(dish.category === "1"){
-                                if(pref && pref.option1 === dish.option){
-                                    dish.isMyOption = true;
-                                } else {
-                                    dish.isNotMyOption = true;
-                                }
-                            }
-
-                            if(dish.category === "2"){
-                                if(pref && pref.option2 === dish.option){
-                                    dish.isMyOption = true;
-                                } else {
-                                    dish.isNotMyOption = true;
-                                }
-                            }
-                        }
-                        
-                    });
-
-
-                    res.render('menu/todaysMenu', context);
-                });
-            } else {
-                res.render('menu/todaysMenu', context);
-            }
+            
+            res.render('menu/todaysMenu', context);
             
         });    
     }
     
     
     menuController.renderNextMenus = function (req, res, next) { 
-        var todayStr = req.query.today || helper.getStringFromDate(new Date());           
+        var todayStr = helper.getRoTodayStr(); // "2016-03-26"           
         menuService.getNextMenus(todayStr, function (err, menus) {
             if(err) { return handleError(res, err); }
 
