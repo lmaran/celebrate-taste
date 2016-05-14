996f4a095f39a1c5aa5110e0136c4337ef9669e1
lmaran <lucian.maran@outlook.com>
2016-05-14 12:57:16 +0000

    emphasize preferences that come from online + auth fix [skip ci]

--- server/config/environment/staging.js
+++ server/config/environment/staging.js
@@ -3,9 +3,9 @@
 
 // Staging specific configuration (declared as "Env. variables" on the remote server)
 module.exports = {
-    port: process.env.PORT,
+    port: process.env.PORT || 1410,
     mongo: {
-        uri: process.env.MONGO_URI
+        uri: process.env.MONGO_URI || 'mongodb://localhost/celebrate-taste-dev'
     },
     gaCode: 'UA-72165579-2',
     externalUrl: 'http://stg.celebrate-taste.ro'   
