141d57b6e8d8312dc5d0483db24f2e5bfd2c2be8
Lucian Maran <lucian.maran@outlook.com>
2016-05-17 23:03:53 +0300

    update angular and all related libraries

--- client/app/common/services/angular-locale_ro-ro.js
+++ client/app/common/services/angular-locale_ro-ro.js
@@ -1,42 +1,69 @@
-// https://code.angularjs.org/1.0.8/i18n/angular-locale_ro-ro.js
-// ultima varianta este aici (dar nu ai nevoie) https://raw.githubusercontent.com/angular/angular.js/master/src/ngLocale/angular-locale_ro-ro.js
+// // https://code.angularjs.org/1.0.8/i18n/angular-locale_ro-ro.js
+// // ultima varianta este aici (dar nu ai nevoie) https://raw.githubusercontent.com/angular/angular.js/master/src/ngLocale/angular-locale_ro-ro.js
 
-// practic ca sa ai localizare in calendarul din AngularUI e suficient sa adaugi acest fisier. Detalii aici: http://stackoverflow.com/a/19676300
+// // practic ca sa ai localizare in calendarul din AngularUI e suficient sa adaugi acest fisier. Detalii aici: http://stackoverflow.com/a/19676300
 
 // jshint ignore: start
 'use strict';
-angular.module('ngLocale', [], ['$provide', function($provide) {
-var PLURAL_CATEGORY = {ZERO: 'zero', ONE: 'one', TWO: 'two', FEW: 'few', MANY: 'many', OTHER: 'other'};
-$provide.value('$locale', {
-  'DATETIME_FORMATS': {
-    'AMPMS': [
-      'AM',
-      'PM'
+angular.module("ngLocale", [], ["$provide", function($provide) {
+var PLURAL_CATEGORY = {ZERO: "zero", ONE: "one", TWO: "two", FEW: "few", MANY: "many", OTHER: "other"};
+function getDecimals(n) {
+  n = n + '';
+  var i = n.indexOf('.');
+  return (i == -1) ? 0 : n.length - i - 1;
+}
+
+function getVF(n, opt_precision) {
+  var v = opt_precision;
+
+  if (undefined === v) {
+    v = Math.min(getDecimals(n), 3);
+  }
+
+  var base = Math.pow(10, v);
+  var f = ((n * base) | 0) % base;
+  return {v: v, f: f};
+}
+
+$provide.value("$locale", {
+  "DATETIME_FORMATS": {
+    "AMPMS": [
+      "a.m.",
+      "p.m."
     ],
-    'DAY': [
-      'duminic\u0103',
-      'luni',
-      'mar\u021bi',
-      'miercuri',
-      'joi',
-      'vineri',
-      's\u00e2mb\u0103t\u0103'
+    "DAY": [
+      "duminic\u0103",
+      "luni",
+      "mar\u021bi",
+      "miercuri",
+      "joi",
+      "vineri",
+      "s\u00e2mb\u0103t\u0103"
     ],
-    'MONTH': [
-      'ianuarie',
-      'februarie',
-      'martie',
-      'aprilie',
-      'mai',
-      'iunie',
-      'iulie',
-      'august',
-      'septembrie',
-      'octombrie',
-      'noiembrie',
-      'decembrie'
+    "ERANAMES": [
+      "\u00eenainte de Hristos",
+      "dup\u0103 Hristos"
     ],
-    'SHORTDAY': [
+    "ERAS": [
+      "\u00ee.Hr.",
+      "d.Hr."
+    ],
+    "FIRSTDAYOFWEEK": 0,
+    "MONTH": [
+      "ianuarie",
+      "februarie",
+      "martie",
+      "aprilie",
+      "mai",
+      "iunie",
+      "iulie",
+      "august",
+      "septembrie",
+      "octombrie",
+      "noiembrie",
+      "decembrie"
+    ],
+    "SHORTDAY": [
       'Du',
       'Lu',
       'Ma',
@@ -45,61 +72,78 @@ $provide.value('$locale', {
       'Vi',
       'S\u00e2'
     ],
-    'SHORTMONTH': [
-      'ian.',
-      'feb.',
-      'mar.',
-      'apr.',
-      'mai',
-      'iun.',
-      'iul.',
-      'aug.',
-      'sept.',
-      'oct.',
-      'nov.',
-      'dec.'
+    "SHORTMONTH": [
+      "ian.",
+      "feb.",
+      "mar.",
+      "apr.",
+      "mai",
+      "iun.",
+      "iul.",
+      "aug.",
+      "sept.",
+      "oct.",
+      "nov.",
+      "dec."
     ],
-    'fullDate': 'EEEE, d MMMM y',
-    'longDate': 'd MMMM y',
-    'medium': 'dd.MM.yyyy HH:mm:ss',
-    'mediumDate': 'dd.MM.yyyy',
-    'mediumTime': 'HH:mm:ss',
-    'short': 'dd.MM.yyyy HH:mm',
-    'shortDate': 'dd.MM.yyyy',
-    'shortTime': 'HH:mm'
+    "STANDALONEMONTH": [
+      "ianuarie",
+      "februarie",
+      "martie",
+      "aprilie",
+      "mai",
+      "iunie",
+      "iulie",
+      "august",
+      "septembrie",
+      "octombrie",
+      "noiembrie",
+      "decembrie"
+    ],
+    "WEEKENDRANGE": [
+      5,
+      6
+    ],
+    "fullDate": "EEEE, d MMMM y",
+    "longDate": "d MMMM y",
+    "medium": "d MMM y HH:mm:ss",
+    "mediumDate": "d MMM y",
+    "mediumTime": "HH:mm:ss",
+    "short": "dd.MM.y HH:mm",
+    "shortDate": "dd.MM.y",
+    "shortTime": "HH:mm"
   },
-  'NUMBER_FORMATS': {
-    'CURRENCY_SYM': 'RON',
-    'DECIMAL_SEP': ',',
-    'GROUP_SEP': '.',
-    'PATTERNS': [
+  "NUMBER_FORMATS": {
+    "CURRENCY_SYM": "RON",
+    "DECIMAL_SEP": ",",
+    "GROUP_SEP": ".",
+    "PATTERNS": [
       {
-        'gSize': 3,
-        'lgSize': 3,
-        'macFrac': 0,
-        'maxFrac': 3,
-        'minFrac': 0,
-        'minInt': 1,
-        'negPre': '-',
-        'negSuf': '',
-        'posPre': '',
-        'posSuf': ''
+        "gSize": 3,
+        "lgSize": 3,
+        "maxFrac": 3,
+        "minFrac": 0,
+        "minInt": 1,
+        "negPre": "-",
+        "negSuf": "",
+        "posPre": "",
+        "posSuf": ""
       },
       {
-        'gSize': 3,
-        'lgSize': 3,
-        'macFrac': 0,
-        'maxFrac': 2,
-        'minFrac': 2,
-        'minInt': 1,
-        'negPre': '-',
-        'negSuf': '\u00a0\u00a4',
-        'posPre': '',
-        'posSuf': '\u00a0\u00a4'
+        "gSize": 3,
+        "lgSize": 3,
+        "maxFrac": 2,
+        "minFrac": 2,
+        "minInt": 1,
+        "negPre": "-",
+        "negSuf": "\u00a0\u00a4",
+        "posPre": "",
+        "posSuf": "\u00a0\u00a4"
       }
     ]
   },
-  'id': 'ro-ro',
-  'pluralCat': function (n) {  if (n == 1) {   return PLURAL_CATEGORY.ONE;  }  if (n == 0 || n != 1 && n == (n | 0) && n % 100 >= 1 && n % 100 <= 19) {   return PLURAL_CATEGORY.FEW;  }  return PLURAL_CATEGORY.OTHER;}
+  "id": "ro-ro",
+  "localeID": "ro_RO",
+  "pluralCat": function(n, opt_precision) {  var i = n | 0;  var vf = getVF(n, opt_precision);  if (i == 1 && vf.v == 0) {    return PLURAL_CATEGORY.ONE;  }  if (vf.v != 0 || n == 0 || n != 1 && n % 100 >= 1 && n % 100 <= 19) {    return PLURAL_CATEGORY.FEW;  }  return PLURAL_CATEGORY.OTHER;}
 });
 }]);
