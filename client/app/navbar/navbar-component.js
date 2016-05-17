23f609cdfd22f9fb114527e8fcf6209ac10bf437
Lucian Maran <lucian.maran@outlook.com>
2016-05-17 23:03:53 +0300

    update angular and all related libraries

---
+++ client/app/navbar/navbar-component.js
@@ -0,0 +1,20 @@
+// (function() {
+//     "use strict";
+    
+//     var module = angular.module("celebrate-taste");
+    
+//     module.component("lmNavbar",{
+//         templateUrl:"app/navbar/navbar-component.html",
+//         controllerAs:"vm",
+//         controller:["$http", controller]
+//     });
+    
+//     function controller($http){
+//         var vm = this;
+        
+//         vm.$onInit = function(){
+            
+//         };
+//     }
+    
+// })()
