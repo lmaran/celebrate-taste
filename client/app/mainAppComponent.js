d0c22b00c5ba1c40c097e80f82a1427c7cd0b123
Lucian Maran <lucian.maran@outlook.com>
2016-05-22 13:26:46 +0300

    rewrite Users as a component; remove security related actions from User (login, register...)

--- client/app/mainAppComponent.js
+++ client/app/mainAppComponent.js
@@ -19,8 +19,15 @@
             //
             {path:"/admin/customerEmployees", component:"customerEmployeeList", name:"CustomerEmployeeList"},
             {path:"/admin/customerEmployees/create", component:"customerEmployee", name:"CustomerEmployeeCreate"},
-            {path:"/admin/customerEmployees/:id", component:"customerEmployee", name:"CustomerEmployeeEdit", data: { action: 'edit'}}
+            {path:"/admin/customerEmployees/:id", component:"customerEmployee", name:"CustomerEmployeeEdit", data: { action: 'edit'}},
             
+            //
+            // users
+            //
+            {path:"/admin/users", component:"userList", name:"UserList"},
+            {path:"/admin/users/create", component:"user", name:"UserCreate"},
+            {path:"/admin/users/:id", component:"user", name:"UserEdit", data: { action: 'edit'}}
+                        
             //{path:"/admin/**", redirectTo:["Home"]}
         ]
     });
