3721aeb7cb20b4f781789ff6657a09819e4537d0
lmaran <lucian.maran@outlook.com>
2016-11-20 22:41:16 +0200

    upload image - wip

--- server/api/dish/dishController.js
+++ server/api/dish/dishController.js
@@ -8,6 +8,8 @@ var sharp = require('sharp');
 var stream = require('stream');
 var fs = require('fs');
 var path = require('path');
+var removeDiacritics = require('diacritics').remove;
+var moment = require('moment');
 
 // ---------- OData ----------
 exports.getAll = function (req, res) {
@@ -72,7 +74,7 @@ exports.remove = function(req, res){
 
 exports.uploadImage = function(req, res){
     
-    var dishesBaseURI = "http://" + config.azureStorage.account + ".blob.core.windows.net/dishes/";
+    var dishesBaseURI = "https://" + config.azureStorage.account + ".blob.core.windows.net/dishes/";
 
     // https://github.com/andrewrk/node-multiparty/blob/master/examples/azureblobstorage.js
     var blobService = azure.createBlobService(config.azureStorage.account, config.azureStorage.key);   
@@ -80,49 +82,119 @@ exports.uploadImage = function(req, res){
 
     form.on('part', function(part) {
         if (!part.filename) return;
+        //console.log(part);
+        //return;
 
         var size = part.byteCount;
-        var blobName = part.filename;
+
+        var parsedFile = path.parse(part.filename); // Cuş Cuş.jpg
+
+        var fileNameWithoutExtension = parsedFile.name; // Cuş Cuş
+        var fileExtensionWithDot = parsedFile.ext; // .jpg
+
+        var blobName = slugify(fileNameWithoutExtension) + fileExtensionWithDot; // cus-cus.jpg
+
         var containerName = 'dishes';
         
-        console.log('original size: ' + size);
+        // console.log('original size: ' + size);
               
         var options = {
             contentSettings:{contentType: part.headers['content-type']}
         };
 
+        // var transformer = sharp()
+        //     .resize(200, 200)
+        //     //.withoutEnlargement()
+        //     // .overlayWith(roundedCorners, { cutout: true })
+        //     // .png();
+        //     .on('info', function (info) {
+        //         console.log('Image height is ' + info.height);
+        //         console.log('Image size is ' + info.size);
+        //         size = info.size;
+
+        //     });
+
+        // var xx = part.pipe(transformer);
+
+        var cb = function(err, result, response) {
+            if (err) {
+                handleError(res, err)
+            }
+            else{               
+                // res.json({url:dishesBaseURI + blobName});
+                console.log('ok');
+            }
+        };
+
+        // var writeStreamToBlob = createWriteStreamToBlockBlob(containerName, blobName, options, cb);
+
+        
+        // https://github.com/Azure/azure-storage-node/issues/102#issuecomment-164234599
+        // part.pipe(transformer).pipe(writeStreamToBlob);
+
+        //var uniqueTime = moment().format("YYMMDDHHmmss"); // 161120172824
+        var uniqueTime = moment().format("HHmmss"); // 172824
+        var blobName = slugify(fileNameWithoutExtension) + '-' + uniqueTime + fileExtensionWithDot;
+
+        var sizes = [{
+            label: "sm",
+            width: 225,
+            height: 150
+        },{
+            label: "lg",
+            width: 960,
+            height: 640
+        }];
+
+        // http://sharp.dimens.io/en/stable/api/#clone
         var transformer = sharp()
-            .resize(200, 200)
-            //.withoutEnlargement()
-            // .overlayWith(roundedCorners, { cutout: true })
-            // .png();
             .on('info', function (info) {
                 console.log('Image height is ' + info.height);
                 console.log('Image size is ' + info.size);
-                size = info.size;
-
+                //size = info.size;
             });
 
-        var xx = part.pipe(transformer);
+        sizes.forEach(function(size){
+            console.log('size=' + size.label);
+            // transformer.clone().resize(size.width, size.height)
+            //     .on('info', function (info) {
+            //         console.log('Image height is ' + info.height);
+            //         console.log('Image size is ' + info.size);
+            //         //size = info.size;
+            //     })
+            //     .pipe(blobService.createWriteStreamToBlockBlob(containerName + '-' + size.label, blobName, options, cb));
 
-        xx.on('end', function(){
-            console.log('new size: ' + size);
+            var xx = transformer.clone().resize(size.width, size.height)
+                .on('info', function (info) {
+                    console.log('Image height is ' + info.height);
+                    console.log('Image size is ' + info.size);
+                    //size = info.size;
+                    
+                });
+            xx.pipe(blobService.createWriteStreamToBlockBlob(containerName + '-' + size.label, blobName, options, cb));
+
         });
 
+        transformer.clone()
+            .pipe(blobService.createWriteStreamToBlockBlob(containerName, blobName, options, cb));
 
-        blobService.createBlockBlobFromStream(containerName, blobName, xx, size, options, function(err, result, response) {
-            if (err) {
-                // error handling
-                // console.log(error);
-                handleError(res, err)
-            }
-            else{
-                // console.log(result);
-                // console.log(response);
+        part.pipe(transformer);
+
+        //saveOriginal();
+
+        // blobService.createBlockBlobFromStream(containerName, blobName, xx, size, options, function(err, result, response) {
+        //     if (err) {
+        //         // error handling
+        //         // console.log(error);
+        //         handleError(res, err)
+        //     }
+        //     else{
+        //         // console.log(result);
+        //         // console.log(response);
                 
-                // res.json({url:dishesBaseURI + blobName});
-            }
-        });
+        //         res.json({url:dishesBaseURI + blobName});
+        //     }
+        // });
 
     });
 
@@ -135,3 +207,18 @@ exports.uploadImage = function(req, res){
 function handleError(res, err) {
     return res.status(500).send(err);
 };
+
+
+function slugify(text) {
+    text = removeDiacritics(text);
+
+    // http://blog.benmcmahen.com/post/41122888102/creating-slugs-for-your-blog-using-expressjs-and
+    return text.toString().toLowerCase()
+      .replace(/_/g, '-')        // Replace _ with -
+      .replace(/\s+/g, '-')        // Replace spaces with -
+      .replace(/[^\w\-]+/g, '')   // Remove all non-word chars
+      .replace(/\-\-+/g, '-')      // Replace multiple - with single -
+      .replace(/^-+/, '')          // Trim - from start of text
+      .replace(/-+$/, '');         // Trim - from end of text
+  }
+
