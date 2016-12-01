1766e72ac10be25d09d5eba2cdf3fa1b6aa0dcea
lmaran <lucian.maran@outlook.com>
2016-12-01 15:17:23 +0200

    (UI) Display a thumbnail in Public zone

--- server/views/menu/menuController.js
+++ server/views/menu/menuController.js
@@ -7,69 +7,86 @@
     var preferenceService = require('../../api/preference/preferenceService');
     var helper = require('../../data/dateTimeHelper');
     var _ = require('lodash');
-    
-    menuController.renderTodaysMenu = function (req, res, next) { 
-        var todayStr = helper.getRoTodayStr(); // "2016-03-26"       
-        menuService.getTodaysMenu(todayStr, function (err, menu) {
-            if(err) { return handleError(res, err); }
+      
+    menuController.renderTodaysMenu = function (req, res, next) {
+        var todayStr = helper.getRoTodayStr(); // "2016-03-26"
+        if(req.user){
+            let p1 = promiseToGetTodaysMenu(todayStr);
+            let p2 = promiseToGetUserPreferenceByDate(req.user.name, todayStr);
+            
+            Promise.all([p1, p2]).then(function(results){
+                let menu = results[0];
+                let pref = results[1];
 
-            var menuHasDishes = menu && menu.dishes && (menu.dishes.length > 0);
-            if(menuHasDishes){                
-                menu.dishes = _.sortBy(menu.dishes, ['category', 'option']);
-                
-                if(req.user && req.user.role === "user"){
-                    
-                    // TODO: run this query in paralel with "getTodaysMenu" 
-                    preferenceService.getByEmployeeAndDate(req.user.name, todayStr, function(err, pref) {
-                        if(err) { return handleError(res, err); }
-
-                        menu.dishes.forEach(function(dish) {
+                var menuHasDishes = menu && menu.dishes && (menu.dishes.length > 0);
+                if(menuHasDishes){                
+                    menu.dishes = _.sortBy(menu.dishes, ['category', 'option']);
+                    menu.dishes.forEach(function(dish) {
+                        
+                        var dishesInCategory = _.filter(menu.dishes, {'category': dish.category});
+                        
+                        if(dishesInCategory.length > 1){
                             
-                            var dishesInCategory = _.filter(menu.dishes, {'category': dish.category});
-                            
-                            if(dishesInCategory.length > 1){
-                                //var pref = _.find(preferences, {'date': menu.menuDate});
-                                
-                                if(pref){
-                                    menu.preferenceId = pref._id;
-                                };
+                            if(pref){
+                                menu.preferenceId = pref._id;
+                            }
 
-                                if(dish.category === "1"){
-                                    if(pref && pref.option1 === dish.option){
-                                        dish.isMyOption = true;
-                                    } else {
-                                        dish.isNotMyOption = true;
-                                    }
-                                }
-
-                                if(dish.category === "2"){
-                                    if(pref && pref.option2 === dish.option){
-                                        dish.isMyOption = true;
-                                    } else {
-                                        dish.isNotMyOption = true;
-                                    }
+                            if(dish.category === "1"){
+                                if(pref && pref.option1 === dish.option){
+                                    dish.isMyOption = true;
+                                } else {
+                                    dish.isNotMyOption = true;
                                 }
                             }
-                            
-                        });
 
-                    });
-                };              
-            };
-            
-            var context = {
-                user: req.user,
-                menu: menu,
-                today: helper.getStringFromString(todayStr),
-                menuHasDishes: menuHasDishes
-            };
-            
-            res.render('menu/todaysMenu', context);
-            
-        });    
+                            if(dish.category === "2"){
+                                if(pref && pref.option2 === dish.option){
+                                    dish.isMyOption = true;
+                                } else {
+                                    dish.isNotMyOption = true;
+                                }
+                            }
+                        }
+                        
+                    });                    
+                }
+
+                var context = {
+                    user: req.user,
+                    menu: menu,
+                    today: helper.getStringFromString(todayStr),
+                    menuHasDishes: menuHasDishes
+                };
+                
+                res.render('menu/todaysMenu', context);
+
+            })
+            .catch(function(err){
+                return handleError(res, err);
+            })
+        } else {
+            promiseToGetTodaysMenu(todayStr).then(function(menu){
+                var menuHasDishes = menu && menu.dishes && (menu.dishes.length > 0);
+                if(menuHasDishes){                
+                    menu.dishes = _.sortBy(menu.dishes, ['category', 'option']);
+                }
+
+                var context = {
+                    user: req.user,
+                    menu: menu,
+                    today: helper.getStringFromString(todayStr),
+                    menuHasDishes: menuHasDishes
+                };
+                
+                res.render('menu/todaysMenu', context);
+
+            })
+            .catch(function(err){
+                return handleError(res, err);
+            })
+        }
     }
-    
-    
+
     menuController.renderNextMenus = function (req, res, next) { 
         var todayStr = helper.getRoTodayStr(); // "2016-03-26"           
         menuService.getNextMenus(todayStr, function (err, menus) {
@@ -88,7 +105,7 @@
                 areMenus: menus && (menus.length > 0)
             };
             
-            if(req.user && req.user.role === "user"){
+            if(req.user){
                 
                 // TODO: run this query in paralel with "getNextMenus" 
                 preferenceService.getByEmployee(req.user.name, todayStr, function(err, preferences) {
@@ -133,7 +150,43 @@
             }
 
         });    
-    }    
+    }  
+
+    function promiseToGetTodaysMenu(todayStr){
+        return new Promise(function(resolve, reject) {
+            menuService.getTodaysMenu(todayStr, function (err, menu) {
+                if(err) { reject(err) }
+                resolve(menu);
+            });  
+        });   
+    }
+
+    function promiseToGetNextMenu(todayStr){
+        return new Promise(function(resolve, reject) {
+            menuService.getNextMenus(todayStr, function (err, menus) {
+                if(err) { reject(err) }
+                resolve(menus);
+            });  
+        });   
+    }
+
+    function promiseToGetUserPreferences(userName, todayStr){
+        return new Promise(function(resolve, reject) {
+            preferenceService.getByEmployee(userName, todayStr, function (err, preferences) {
+                if(err) { reject(err) }
+                resolve(preferences);  
+            });
+        });
+    }   
+
+    function promiseToGetUserPreferenceByDate(userName, todayStr){
+        return new Promise(function(resolve, reject) {
+            preferenceService.getByEmployeeAndDate(userName, todayStr, function (err, preference) {
+                if(err) { reject(err) }
+                resolve(preference);  
+            });
+        });
+    }         
     
     function handleError(res, err) {
         return res.status(500).send(err);
