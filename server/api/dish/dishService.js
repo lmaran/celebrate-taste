bfe2588eb6a09f09003dd6ec91c8457b82e9c2a7
lmaran <lucian.maran@outlook.com>
2016-12-02 18:17:13 +0200

    (Backend) Use Mongodb to keep a journal for each uploaded image

--- server/api/dish/dishService.js
+++ server/api/dish/dishService.js
@@ -2,8 +2,10 @@
 
 (function (dishService) {
     
-   var mongoService = require('../../data/mongoService');
+    var mongoService = require('../../data/mongoService');
+    var mongoHelper = require('../../data/mongoHelper');
     var collection = 'dishes';
+    var imageCollection = 'dishImages';
  
  
     // ---------- OData ----------
@@ -19,12 +21,12 @@
         mongoService.getById(collection, id, next);
     };
 
-    dishService.create = function (badge, next) {
-        mongoService.create(collection, badge, next);
+    dishService.create = function (dish, next) {
+        mongoService.create(collection, dish, next);
     };
 
-    dishService.update = function (badge, next) {        
-        mongoService.update(collection, badge, next);
+    dishService.update = function (dish, next) {        
+        mongoService.update(collection, dish, next);
     };  
 
     dishService.remove = function (id, next) {
@@ -35,6 +37,21 @@
     // ---------- RPC ----------    
     dishService.getByValue = function (field, value, id, next) {
         mongoService.getByValue(collection, field, value, id, next);
-    };    
+    };  
+
+    dishService.createImageEntity = function (dishImage, next) {
+        mongoService.create(imageCollection, dishImage, next);
+    };
+
+    dishService.getImageEntity = function (blobName, next) {
+        mongoHelper.getDb(function (err, db) {
+            if (err) return next(err, null);
+            db.collection(imageCollection).findOne({blobName: blobName}, next);                           
+        });        
+    };
+
+    dishService.updateImageEntity = function (dishImage, next) {        
+        mongoService.update(imageCollection, dishImage, next);
+    };              
     
 })(module.exports);
