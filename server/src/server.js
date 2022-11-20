d3ff3d5e3324c40255a3aabb4aa7ac8a78522087
Lucian Maran <lucian.maran@outlook.com>
2022-11-20 21:19:44 +0200

    bug fix

--- server/src/server.js
+++ server/src/server.js
@@ -6,8 +6,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 var express = require("express");
 var config = require("./config/environment");
-var logger = require("./logging/logger");
-var errorLogHandler = require("./logging/errorLogHandler");
+//var logger = require("./logging/logger");
+//var errorLogHandler = require("./logging/errorLogHandler");
 
 var app = express();
 
@@ -16,11 +16,12 @@ require("./app.js")(app);
 require("./routes")(app);
 
 // Handle error has to be last: http://expressjs.com/en/guide/error-handling.html
-app.use(errorLogHandler());
+//app.use(errorLogHandler());
 
 // Start server
 app.listen(config.port, config.ip, function() {
-    logger.warn(`Express server listening on ${config.port} in ${config.env} mode`);
+    //logger.warn(`Express server listening on ${config.port} in ${config.env} mode`);
+    console.log(`Express server listening on ${config.port} in ${config.env} mode`);
 });
 
 // Expose app
